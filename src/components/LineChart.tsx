f96e17f1ee06358e978a6b35f241bd76007100a8
JayKrishnaMandal <80018947+JayKrishnaMandal@users.noreply.github.com>
2024-05-01 20:43:11 +0545

    added graph

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -7,6 +7,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+
 ChartJs.register(Legend, LineElement, LinearScale, CategoryScale, PointElement);
 
 const data = {
@@ -26,18 +27,11 @@ const data = {
   ],
   datasets: [
     {
-      label: "Monthly exam",
-      data: [33, 53, 85, 41, 44, 65, 86, 74, 19, 56, 87, 90, 100],
+      label: "Monthly-Exam",
+      data: [33, 53, 85, 41, 44, 65, 86, 74, 19, 56, 87, 90],
       fill: true,
       backgroundColor: "rgba(75,192,192,0.2)",
       borderColor: "rgba(75,192,192,1)",
-      tension: 0.4,
-    },
-    {
-      label: "Quaterly exam ",
-      data: [33, 25, 35, 51, 54, 76, 100, 99, 57, 89, 89, 100],
-      fill: false,
-      borderColor: "#742774",
       tension: 0.4,
     },
   ],
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,6 @@
 import "./Dashboard.css";
 
 import Dashboard from "./Dashboard";
-import LineChart from "../components/LineChart";
 
 const Layout = () => {
   return (
